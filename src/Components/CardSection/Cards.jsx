67ad9c3d054c0f00566f4e637046b4802b348101
alsafa012 <alsafa012@gmail.com>
2023-09-25 01:01:56 +0600

    json data fetch amd loaded successfully and also show all cards successfully

--- src/Components/CardSection/Cards.jsx
+++ src/Components/CardSection/Cards.jsx
@@ -10,6 +10,7 @@ const Cards = ({ category }) => {
           text_button_bg,
           description,
           price,
+          category_name,
      } = category;
      return (
           <div>
@@ -17,21 +18,21 @@ const Cards = ({ category }) => {
                     <figure>
                          <img
                               src={picture}
-                              alt="Shoes"
+                              alt="Categories"
                          />
                     </figure>
                     <div className="card-body">
-                         <h2 className="card-title">
-                              {title}
-                              <div className="badge badge-secondary">NEW</div>
+                         <h2 className="card-title text-sm font-medium">
+                              {category_name}
+                              
                          </h2>
-                         <p>If a dog chews shoes whose shoes does he choose?</p>
-                         <div className="card-actions justify-end">
+                         <p className="font-semibold text-xl">{title}</p>
+                         {/* <div className="card-actions justify-end">
                               <div className="badge badge-outline">Fashion</div>
                               <div className="badge badge-outline">
                                    Products
                               </div>
-                         </div>
+                         </div> */}
                     </div>
                </div>
           </div>
